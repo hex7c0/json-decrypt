9026af93fcc997c047bb6b2aab12355fd3b147f1
hex7c0 <carniellifrancesco@gmail.com>
2017-01-22 03:28:18 +0100

    0.2.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @file json-decrypt main
  * @module json-decrypt
  * @subpackage main
- * @version 0.1.0
+ * @version 0.2.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
